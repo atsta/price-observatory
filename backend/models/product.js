bef8b5020be45624152f4619228823134f8a38ed
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2018-12-30 13:17:43 +0200

    Test product model

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,9 +3,35 @@ const BaseModel = require('./base')
 module.exports = class Product extends BaseModel {
     constructor(connection) {
         super('products', connection)
-    }
-
-    async list() {
-        return await this.execute(`SELECT * FROM ${this.table}`)
+        this.options = {
+            //table: 'products',
+            //optionalField: {query: 'employeeId', field_name: 'employee_id'},
+            list: {
+                allowed_search_keys: [
+                    'product_id',
+                    'name',
+                    'barcode'
+                ],
+                fields: [],
+                //orderBy: [order_field('name', 'ASC'), order_field('last_name', 'ASC')]
+            },
+            insert: {
+                fields: [
+                    'product_id',
+                    'name',
+                    'barcode'
+                ]
+            },
+            update: {
+                fields: [
+                    'product_id',
+                    'name',
+                    'barcode',
+                ]
+            },
+            delete: {
+                queryField: 'id'
+            }
+        }
     }
 }
